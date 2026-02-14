1b2802efaaa663012ea9e4b369a2b77cd3b17e58
Richard <bubzyg@hotmail.com>
2026-02-14 14:14:34 +0000

    added bot code

--- bot.py
+++ bot.py
@@ -1,2 +1,20 @@
 import os
+import discord
+from discord import app_commands
+
+class Bot(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.default()
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        await self.tree.sync()  # sync globally
+
+bot = Bot()
+
+@bot.tree.command(name="ping", description="Check if bot works")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("pong", ephemeral=True)
+    
 bot.run(os.environ["DISCORD_TOKEN"])
